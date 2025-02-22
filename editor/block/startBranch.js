a26c60a6a87b321be3e303b00378c12750c5c363
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-02-22 15:29:32 -0400

    Update startBranch.js

--- editor/block/startBranch.js
+++ editor/block/startBranch.js
@@ -1,10 +1,10 @@
 Blockly.Blocks['start_branch'] = {
   init: function() {
-    this.appendValueInput('branch')
-      .setCheck("Number")
-      .appendField('start branch');
-    this.appendValueInput('bool')
-      .setCheck("Bool")
+    this.appendDummyInput()
+      .appendField("start branch")
+      .appendField(new Blockly.FieldTextInput("1"), "BRANCH")
+      .appendField(" ")
+      .appendField(new Blockly.FieldTextInput("Toggle?"), "BOOL");
     this.setPreviousStatement(true, null);
     this.setColour(0);
     this.setTooltip('Starts a branches code in the blocks function I think');
@@ -13,8 +13,8 @@ Blockly.Blocks['start_branch'] = {
 };
 
 javascript.javascriptGenerator.forBlock['start_branch'] = function(block) {
-  const branch = Blockly.JavaScript.valueToCode(block, 'branch', Blockly.JavaScript.ORDER_NONE);
-  const bool = Blockly.JavaScript.valueToCode(block, 'bool', Blockly.JavaScript.ORDER_NONE);
+  const branch = Blockly.JavaScript.valueToCode(block, 'BRANCH', Blockly.JavaScript.ORDER_NONE);
+  const bool = Blockly.JavaScript.valueToCode(block, 'BOOL', Blockly.JavaScript.ORDER_NONE);
 
   const code = `util.startBranch(${branch}, ${bool})`;
 
