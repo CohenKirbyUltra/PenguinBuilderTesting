19ca60736eafa9ec9d99d0a3a8cfb7de3c285bd1
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-02-22 06:24:42 -0400

    Update looper.js

--- editor/block/looper.js
+++ editor/block/looper.js
@@ -1,1 +1,15 @@
-temp
+Blockly.Blocks['looper'] = {
+  init: function() {
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField("Stack Timer Finished?");
+    this.appendDummyInput()
+    this.setColour(0);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+
+javascript.javascriptGenerator.forBlock['looper'] = function(block) {
+  return util.stackTimerFinished();
+};
