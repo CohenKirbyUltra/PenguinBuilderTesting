23f5e23eb112ac9020f38114fc7aa9e0cd2960a4
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-02-23 02:54:52 -0400

    Update sketch.js

--- editor/sketch.js
+++ editor/sketch.js
@@ -676,7 +676,10 @@ function viewCode(content, filename, contentType = "text/plain") {
 
     const a = document.createElement("a");
     const url = window.URL.createObjectURL(blob);
-    window.open(url);
+        window.open(
+            "https://studio.penguinmod.com/editor?extension=" +
+            encodeURIComponent("data:text/plain;base64," + content, '_blank'
+        ).focus();
     setTimeout(window.URL.revokeObjectURL(url), 10000);
 }
 
